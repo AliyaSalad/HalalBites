9be059a7e8d8596637188e8c4fb25c20b6beed78
AliyaSalad <159142561+AliyaSalad@users.noreply.github.com>
2024-02-29 17:45:36 +0000

    Admin (#3)
    * Images
    
    * Images
    
    * colour changes
    
    * categories

---
+++ src/components/UI/Form 2.js
@@ -0,0 +1,82 @@
+import { KeyboardAvoidingView, ScrollView, StyleSheet, Text, TextInput, View  } from "react-native";
+import { Button, ButtonTray } from "./Buttons";
+import Icons from "./Icons";
+
+const Form = ({children, onSubmit, onCancel, submitLabel, submitIcon}) => {
+    //Initialisation
+    //State
+    //Handlers
+    //View
+
+    return (
+        <KeyboardAvoidingView style={styles.formContainer}>
+
+            <ScrollView contentContainerStyle={styles.formItems}>
+                {children}
+            </ScrollView>
+
+            <ButtonTray>
+                <Button label = {submitLabel} icon={submitIcon} onClick={onSubmit} />
+                <Button label = 'Cancel' icon={<Icons.Close />} onClick={onCancel} />
+        </ButtonTray>
+
+        </KeyboardAvoidingView>
+    );
+};
+
+
+
+const InputText = ({label, value, onChange}) => {
+    //Initialisation
+    //State
+    //Handlers
+    //View
+    return (
+        <View style={styles.item}>
+            <Text style={styles.itemLabel}>{label}</Text>
+            <TextInput 
+                value={value} 
+                onChangeText={onChange} 
+                style={styles.itemTextInput} 
+            />
+        </View>
+    );
+};
+
+
+//Compose components
+
+Form.InputText = InputText;
+
+
+
+//Styles
+
+const styles = StyleSheet.create({
+    formContainer: {
+        gap: 10,
+    },
+    itemLabel: {
+        color:'grey',
+        fontSize: 16,
+        marginBottom: 5,
+      },
+      itemTextInput: {
+        height: 50,
+        paddingLeft: 10,
+        fontSize: 16,
+        backgroundColor: 'white',
+        borderRadius: 7,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+      },
+      itemPickerStyle: {
+        height: 50,
+        backgroundColor: 'whitesmoke',
+      },
+      itemPickerPromptStyle: {
+        color: 'gray',
+      },
+});
+
+export default Form;
